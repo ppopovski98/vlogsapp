0b91bbb66bcb88c90df3d92778762ac2bb092bc9
ppopovski98 <petar@wolfwarelabs.com>
2023-06-05 16:46:26 +0200

    Log in screen: Snapkit constraints, layout and colors.

--- vlogsApp/vlogsApp/Controller/SignInViewController.swift
+++ vlogsApp/vlogsApp/Controller/SignInViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 import FirebaseAuth
+import SnapKit
 
 class SignInViewController: UIViewController {
     
-    let loginButton = UIButton(type: .system)
+    let loginButton = UIButton(type: .roundedRect)
     let emailTextField = UITextField()
     let passwordTextField = UITextField()
-
+    let noCredentialsAlert = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,58 +23,100 @@ class SignInViewController: UIViewController {
     }
     
     
+    //Config for the buttons, style and colors.
+    
     func configUI() {
         
+        let attributes: [NSAttributedString.Key: Any] = [ NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.3)]
+        let emailAttributedTextField = NSAttributedString(string: emailTextField.placeholder ?? "E-mail", attributes: attributes)
+        let passwordAttributedTextField = NSAttributedString(string: passwordTextField.placeholder ?? "Password", attributes: attributes)
+        
+        view.backgroundColor = UIColor(named: "backgroundColor")
+        
         loginButton.setTitle("Log In", for: .normal)
-        loginButton.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        loginButton.backgroundColor = UIColor(named: "textFieldColor")
+        loginButton.layer.cornerRadius = 10
+        loginButton.tintColor = .white
+        loginButton.frame = CGRect(x: 100, y: 100, width: 350, height: 50)
         loginButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginButton)
         
-        emailTextField.frame = CGRect(x: 100, y: 100, width: 200, height: 30)
+        emailTextField.frame = CGRect(x: 100, y: 100, width: 350, height: 40)
         emailTextField.placeholder = "E-mail"
+        emailTextField.attributedPlaceholder = emailAttributedTextField
+        emailTextField.textColor = .white
         emailTextField.borderStyle = .roundedRect
+        emailTextField.backgroundColor = UIColor(named: "textFieldColor")
         view.addSubview(emailTextField)
         
-        passwordTextField.frame = CGRect(x: 100, y: 150, width: 200, height: 30)
+        passwordTextField.frame = CGRect(x: 100, y: 150, width: 350, height: 40)
         passwordTextField.placeholder = "Password"
+        passwordTextField.attributedPlaceholder = passwordAttributedTextField
+        passwordTextField.textColor = .white
         passwordTextField.borderStyle = .roundedRect
+        passwordTextField.backgroundColor = UIColor(named: "textFieldColor")
         passwordTextField.isSecureTextEntry = true
         view.addSubview(passwordTextField)
         
-        let constraints = [
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor),
-            loginButton.widthAnchor.constraint(equalToConstant: 200),
-            loginButton.heightAnchor.constraint(equalToConstant: 50),
-            
-            emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            emailTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor),
-            emailTextField.widthAnchor.constraint(equalToConstant: 200),
-            emailTextField.heightAnchor.constraint(equalToConstant: 30),
-            
-            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor),
-            passwordTextField.widthAnchor.constraint(equalToConstant: 200),
-            passwordTextField.heightAnchor.constraint(equalToConstant: 30)
-        ]
-        NSLayoutConstraint.activate(constraints)
+        noCredentialsAlert.isHidden = true
+        noCredentialsAlert.textColor = .red
+        noCredentialsAlert.text = "Incorrect e-mail and/or password."
+        noCredentialsAlert.textAlignment = .center
+        view.addSubview(noCredentialsAlert)
+        
+        // These are the constraints for the mail, password and log in button using snapkit.
+        
+        loginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(passwordTextField.snp.bottom).offset(10)
+            make.width.equalTo(350)
+            make.height.equalTo(50)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(passwordTextField.snp.top).offset(-10)
+            make.width.equalTo(350)
+            make.height.equalTo(40)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalTo(350)
+            make.height.equalTo(40)
+        }
+        
+        noCredentialsAlert.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(loginButton.snp.bottom).offset(5)
+            make.width.equalTo(350)
+            make.height.equalTo(50)
+        }
     }
     
+    //Firebase Auth
+    
     @objc func buttonTapped() {
+        
         guard let email = emailTextField.text, !email.isEmpty,
               let password = passwordTextField.text, !password.isEmpty else {
+            self.noCredentialsAlert.isHidden = false
             return
         }
         
         FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
             
-            if error != nil {
+        if error != nil {
                 print(error?.localizedDescription ?? "Error")
             } else {
                 print("Success")
             }
         })
     }
+    
+    //To add: another view controller when you pass the log in screen, or when you sign up.
 }
+
 
